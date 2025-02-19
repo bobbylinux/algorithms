4ecb8b050f82fd6440d95ffebd5c7017272e4b2d
Roberto Bani <roberto.bani@gmail.com>
2025-02-19 15:31:55 +0100

    implemented recursive fibonacci and memoization fibonacci

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -1,11 +1,27 @@
 def n_sum(n):
-    result = [0 for _ in range(n+1)]
+    result = [0 for _ in range(n + 1)]
     result[0] = 0
-    for i in range(1, n+1):
-        result[i] = result[i-1] + i
+    for i in range(1, n + 1):
+        result[i] = result[i - 1] + i
     return result[n]
 
 
+def fibonacci(n):
+    if n <= 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def memFibonacci(n, f):
+    if n <= 1:
+        return 1
+    if f[n-1] == 0:
+        f[n-1] = memFibonacci(n - 1, f) + memFibonacci(n - 2, f)
+    return f[n-1]
+
+
 if __name__ == '__main__':
-    print(n_sum(1))
-    
+    # print(n_sum(1))
+    # print(fibonacci(5))
+    n = 5
+    print(memFibonacci(n, [0] * n))
