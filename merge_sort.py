8a0b631e3165ffc5b431d00a0ba3b2958969478d
Roberto Bani <roberto.bani@gmail.com>
2025-03-01 15:05:37 +0100

    implemented merge sort with extend naif

--- merge_sort.py
+++ merge_sort.py
@@ -1,16 +1,32 @@
-def merge_sort(arr, start, end):
-    if start < end:
-        mid = (start + end) // 2
-        merge_sort(arr, start, mid)
-        merge_sort(arr, mid + 1, end)
-        merge(arr, start, mid, end)
+def merge_sort(l):
+    if len(l) <= 1:
+        return l
+    m = len(l) // 2
+    left = l[:m]
+    right = l[m:]
+    ll = merge_sort(left)
+    lr = merge_sort(right)
+    return merge(ll, lr)
 
 
-def merge(a, s, m, e):
-    return
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    for x in left[i:]:
+        result.append(x)
+    for y in right[j:]:
+        result.append(y)
+    return result
 
 
 if __name__ == '__main__':
-    a1 = [11,3,7,12,15,4,8]
-    merge_sort(a1, 0, len(a1)-1)
-    print(a1)
+    lista = [11, 3, 7, 12, 15, 4, 8]
+    print(merge_sort(lista))
+
