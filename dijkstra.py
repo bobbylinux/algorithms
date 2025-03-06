d97e3e26ed45df79c010c96c7d459cb5c8991c76
Roberto Bani <roberto.bani@gmail.com>
2025-03-06 20:54:53 +0100

    added a Monti version of dijkstra with arrays (O(n^2))

--- dijkstra.py
+++ dijkstra.py
@@ -1,5 +1,6 @@
 import heapq
 
+
 # questa versione fa uso di heap, ed è la migliore scelta per grafi sparsi (complessità O[(n*m)*log(n)]
 def dijkstra(l_adj, start):
     # Dizionario delle distanze con valori iniziali a +infinito
@@ -25,15 +26,46 @@ def dijkstra(l_adj, start):
     return dists
 
 
+def dijkstra_monti(adj, start):
+    distances = [float('inf')] * len(adj)
+    fathers = [-1] * len(adj)
+    nodes = [x for x in adj]
+    fathers[start] = start
+    distances[start] = 0
+    while nodes:
+        m = float('inf')
+        node = -1
+        for a in nodes:
+            if distances[a] < m:
+                m = distances[a]
+                node = a
+        nodes.pop(node)
+        for n, d in adj[node]:
+            if distances[node] + d < distances[n]:
+                fathers[n] = node
+                distances[n] = distances[node] + d
+
+    return distances
 
 
 if __name__ == '__main__':
-    l_adj = {
-        'A': [('B', 1), ('C', 4)],
-        'B': [('A', 1), ('C', 2), ('D', 5)],
-        'C': [('A', 4), ('B', 2), ('D', 1)],
-        'D': [('B', 5), ('C', 1)]
+    # l_adj = {
+    #     'A': [('B', 1), ('C', 4)],
+    #     'B': [('A', 1), ('C', 2), ('D', 5)],
+    #     'C': [('A', 4), ('B', 2), ('D', 1)],
+    #     'D': [('B', 5), ('C', 1)]
+    # }
+    #
+    # start = 'A'
+    # print(dijkstra(l_adj, start))
+
+    adj = {
+        0: [(1, 4), (2, 17)],
+        1: [(0, 4), (5, 1), (2, 6)],
+        2: [(1, 17), (1, 6), (3, 5)],
+        3: [(2, 5), (4, 10), (5, 4)],
+        4: [(3, 10), (5, 12)],
+        5: [(1, 1), (3, 4), (4, 12)]
     }
 
-    start = 'A'
-    print(dijkstra(l_adj, start))
+    print(dijkstra_monti(adj, 0))
