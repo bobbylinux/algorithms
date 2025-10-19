5aed89db125ae735956fecce589b4c8ce4c49da9
Roberto Bani <roberto.bani@gmail.com>
2025-10-19 19:09:24 +0200

    optimized permutation swap

--- backtracking.py
+++ backtracking.py
@@ -11,29 +11,39 @@ def permutation_naif(numbers, partials):
 
 
 def permutation_swap(numbers, i):
-    if i == len(numbers):
+    if i == 0:
         print(numbers)
         return
 
-    for j in range(i, len(numbers)):
-        numbers[i], numbers[j] = numbers[j], numbers[i]
-        permutation_swap(numbers, i+1)
-        numbers[i], numbers[j] = numbers[j], numbers[i]
+    for j in range(i):
+        numbers[i-1], numbers[j] = numbers[j], numbers[i-1]
+        permutation_swap(numbers, i - 1)
+        numbers[i-1], numbers[j] = numbers[j], numbers[i-1]
 
 
 def subsets_rec(n, current, i):
     if i > n:
         # condizione di stop: ho considerato tutti gli elementi
         print(current)
-        return 
-    # caso 1: NON includo i
-    subsets_rec(n, current, i+1)
+        return
+        # caso 1: NON includo i
+    subsets_rec(n, current, i + 1)
 
     # caso 2: includo i
-    subsets_rec(n, current + [i], i+1)
+    subsets_rec(n, current + [i], i + 1)
+
+
+def subsets_bitmask(n):
+    for j in range(2 ** (n - 1)):
+        print("{", end=" ")
+        for i in range(n - 1):
+            if j & 2 ** i != 0:
+                print(i, ",", end=" ")
+        print("}")
 
 
 if __name__ == '__main__':
     # permutation_naif([1,2,3],[])
-    #permutation_swap([1,2,3],0)
-    subsets_rec(3, [], 1)
+    permutation_swap([1,2,3],3)
+    # subsets_rec(3, [], 1)
+    # subsets_bitmask(4)
