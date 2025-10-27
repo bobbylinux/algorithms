84543d2929389ac44904ed2375f439eb13b47b69
roberto <roberto.bani@gmail.com>
2025-10-27 09:36:45 +0100

    added exams exercises

--- backtracking.py
+++ backtracking.py
@@ -16,9 +16,9 @@ def permutation_swap(numbers, i):
         return
 
     for j in range(i):
-        numbers[i-1], numbers[j] = numbers[j], numbers[i-1]
+        numbers[i - 1], numbers[j] = numbers[j], numbers[i - 1]
         permutation_swap(numbers, i - 1)
-        numbers[i-1], numbers[j] = numbers[j], numbers[i-1]
+        numbers[i - 1], numbers[j] = numbers[j], numbers[i - 1]
 
 
 def subsets_rec(n, current, i):
@@ -33,6 +33,24 @@ def subsets_rec(n, current, i):
     subsets_rec(n, current + [i], i + 1)
 
 
+def subsets_naif(n, current, i):
+    if i > n:
+        print(current)
+        return
+    subsets_naif(n, current, i + 1)
+    subsets_naif(n, current + [i], i + 1)
+
+
+def subsets_k_of_n(n, missing, current, i):
+    if missing == 0:
+        print(current)
+        return
+    # se non ho superato n e missing è maggiore di zero e minore delle scelte ancora da fare
+    elif i <= n and 0 < missing <= n - (i - 1):
+        subsets_k_of_n(n, missing, current, i + 1)
+        subsets_k_of_n(n, missing - 1, current + [i], i + 1)
+
+
 def subsets_bitmask(n):
     for j in range(2 ** (n - 1)):
         print("{", end=" ")
@@ -42,8 +60,50 @@ def subsets_bitmask(n):
         print("}")
 
 
+def ex03_0925(n, s):
+    if len(s) == n:
+        print(s)
+    for j in (0, 1, 2):
+        if len(s) < 1 or (len(s) == 1 and s[0] + j >= 2) or (len(s) >= 2 and 4 <= j + s[-1] + s[-2] <= 5):
+            s.append(j)
+            ex03_0925(n, s)
+            s.pop()
+
+
+def ex03_0724(n, i, s, pb, cb):
+    if n == i:
+        print(s)
+        return
+    for x in {0, 1}:
+        if cb is None:
+            cb = {"k": x, "v": 1}
+            s.append(x)
+            ex03_0724(n, i + 1, s, pb, cb)
+            s.pop(x)
+        else:
+            if x == cb["k"]:
+                cb["v"] += cb["v"] + 1
+                s.append(x)
+                ex03_0724(n, i + 1, s, pb, cb)
+                s.pop(x)
+            else:
+                pb = cb
+                cb = {"k": x, "v": 1}
+                s.append(x)
+                ex03_0724(n, i + 1, s, pb, cb)
+                s.pop(x)
+        if pb is not None and cb is not None and cb["v"] <= pb["v"]:
+            s.append(x)
+            ex03_0724(n, i + 1, s, pb, cb)
+            s.pop(x)
+
+
 if __name__ == '__main__':
     # permutation_naif([1,2,3],[])
-    permutation_swap([1,2,3],3)
+    # permutation_swap(['A', 'B', 'C'], 3)
     # subsets_rec(3, [], 1)
+    # subsets_k_of_n(6, 3,[], 1)
+    # subsets_naif(3,[], 1)
     # subsets_bitmask(4)
+    # c = ex03_0925(5, [])
+    ex03_0724(4, 0, [], None, None)
--- dynamic_programming.py
+++ dynamic_programming.py
@@ -27,7 +27,7 @@ def fibonacci_memoization(n, F):
     return F[n]
 
 
-def iterFibonacci(n):
+def iter_fibonacci(n):
     f = [0] * (n + 1)
     f[0] = 1
     f[1] = 1
@@ -61,26 +61,54 @@ def file(s, k, cap):
     return maxim
 
 
+def hateville(D):
+    DP = [0] * (len(D) + 1)
+    DP[0] = 0
+    DP[1] = D[0]
+    for i in range(2, len(DP)):
+        DP[i] = max(DP[i - 1], DP[i - 2] + D[i - 1])
+    return DP
+
+
+def hateville_solution(DP, D, i):
+    if i == 0:
+        return []
+    if i == 1:
+        return [0]
+    if DP[i] == DP[i-1]:
+        return hateville_solution(DP, D, i-1)
+    else:
+        result = hateville_solution(DP, D, i-2)
+        result.append(i-1)
+    return result
+
+def hateville2(D):
+    DP = [0] * (len(D) + 1)
+    DP[0] = 0
+    DP[1] = D[0]
+    for i in range(2, len(DP)):
+        DP[i] = max(DP[i - 1], DP[i - 2] + D[i - 1])
+    return hateville_solution(DP, D, len(D))
+
 def knapsack(weigth, value, capacity):
     result = [[]]
     n = len(weigth)
     result = [[0 for _ in range(capacity+1)] for _ in range(n+1)]
-    for i in range(1, n+1): 
-        for c in range(1, capacity+1): 
-            if weigth[i-1] <= c: 
+    for i in range(1, n+1):
+        for c in range(1, capacity+1):
+            if weigth[i-1] <= c:
                 result[i][c] = max(result[i-1][c], result[i-1][c-weigth[i-1]] + value[i-1])
-            else: 
+            else:
                 result[i][c] = result[i-1][c]
     return result[n][capacity]
 
 
 if __name__ == '__main__':
     # print(n_sum(1))
-    # s = [1024, 100, 101, 2048, 1984, 55, 1, 18]
-    # file(s, len(s), 5120)
+    s = [1024, 100, 101, 2048, 1984, 55, 1, 18]
+    file(s, len(s), 5120)
     # n = 5
     # print(fibonacci(n))
     # print(memFibonacci(n, [0] * n))
     # print(iterFibonacci(n))
     # print(fibonacci_iterativo(n))
-    print(knapsack([4,2,3,4], [10,7,8,6], 9))
